790eb9cd9baa168d5ef83bc4ccd81e022d0f4a38
polypmer <exorable.ludos@gmail.com>
2016-09-15 14:21:06 -0400

    Add broken template

--- stream.go
+++ stream.go
@@ -2,10 +2,20 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"io/ioutil"
 	"net/http"
 	"os"
 )
+
+type Playlist struct {
+	Songs []string
+}
+
+func (p *Playlist) playlistHandler(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("playlist.html")
+	t.Execute(w, p)
+}
 
 func main() {
 	var dir string // to serve
@@ -19,12 +29,16 @@ func main() {
 	if err != nil {
 		fmt.Println(err)
 	}
+
+	p := new(Playlist)
+	p.Songs = make([]string, len(files))
 	for _, f := range files {
-		fmt.Println(f.Name())
+		// fmt.Println(f.Name())
+		p.Songs = append(p.Songs, f.Name())
 	}
 
 	fs := http.FileServer(http.Dir(dir))
 	http.Handle("/media", http.StripPrefix("/media", fs))
-
+	http.HandleFunc("/", p.playlistHandler)
 	http.ListenAndServe(":5177", nil)
 }
